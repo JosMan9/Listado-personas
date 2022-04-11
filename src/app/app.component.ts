06fa083e43cb5e4282fa0409a416bacd624f80a8
Manuel Rosas <josem_rl@hotmail.com>
2022-04-11 11:03:43 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de personas';
+  nombreInput = '';
+  apellidoInput = '';
+
+  personas: Persona[] = [new Persona('José', "Rosas"), new Persona('María','López')];
+
+  agregarPersona() {
+    let persona = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona);
+    this.nombreInput = '';
+    this.apellidoInput = '';
+  }
 }
